5ccbbae02d9ec26e05c23d6ad8fab208c9bf4253
zber <zber@zhaw.ch>
2020-05-08 09:13:11 +0200

    full statistic run over one server

---
+++ statistics/queryAll.36.py
@@ -0,0 +1,188 @@
+import requests, os, re, json, sys
+import argparse
+import urllib.parse
+from pprint import pprint
+import datetime
+
+if __name__ == '__main__':
+   parser = argparse.ArgumentParser()
+   parser.add_argument('--loops')
+   parser.add_argument('--outputfile')
+   parser.add_argument('--inputfile')
+   args = parser.parse_args()
+
+
+url0 = 'http://biosoda-prod.cloudlab.zhaw.ch:8080/rdf4j-server/repositories/bgeelight'
+
+url1_title = "mySQL"
+url1 = url0
+
+header_title = "header"
+header = "header"
+
+intermed = "?query="
+restofurl = "&format=JSON&limit=100&offset=0&inference=false"
+
+# the separator we use in our json
+separator = '$$'
+
+# add inner limits? leave an empty string if no limits are wanted
+addlimitsnumber = 10
+addlimits = 'LIMIT ' + str(addlimitsnumber)
+addlimits10  = 'LIMIT ' + str(addlimitsnumber * 10)
+
+# how many requests should be done:
+loops = 3
+if None != args.loops:
+    loops = int(args.loops)
+# verbose means, that the logfile gets verbose. console is always verbose
+verbose = 0
+
+# open statistics file
+outputfile = "statisticsall"
+if None != args.outputfile:
+    outputfile = args.outputfile
+outputfile = outputfile + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+f = open(outputfile, "w")
+f.close()
+
+# pstring: what to print
+# thiverbose: should the string be printed into logfile also
+# newline: what is to be printed after the string (typically newline)
+# printbefore: what is to be printed before string
+def printwrite(pstring, thisverbose = 1, newline = "\n", printbefore = ""):
+    f = open(outputfile, "a")
+    tmpstr = str(pstring)
+    print(tmpstr)
+    if thisverbose == 1:
+        f.write(printbefore + tmpstr + newline)
+    f.close();
+
+# where to get the .rq files from
+targetfile = '../biosoda_frontend/src/biosodadata.json'
+if None != args.inputfile:
+    targetfile = args.inputfile
+
+# on the server we deliver the json file next to the script:
+# targetfile = 'biosodadata.json'
+
+# initialise statistics variable
+sums = {}
+
+printwrite('LIMITS: ' + addlimits)
+printwrite('LIMITS10: ' + addlimits10)
+printwrite('url0, 1: \n' + url0 + '\n' + url1 + ' (' + url1_title + ')')
+printwrite('Loops: ' + str(loops))
+
+with open(targetfile) as jsondata:
+    data = json.load(jsondata)
+    for onequery in data['questions']:
+        printwrite("===============", 1, "\n", "\n")
+        queryid = onequery['id']
+        printwrite(queryid)
+
+        if not queryid == 'uniprotQ3':
+            nodebug = 1
+            # continue # debug only
+
+        if not 'SPARQL' in onequery:
+            printwrite('no query')
+            continue
+
+        # prepare statistics
+        sums[queryid] = {}
+
+        sums[queryid]['header'] = {}
+        sums[queryid]['url1'] = {}
+
+        sums[queryid]['header']['total'] = 'total'
+        sums[queryid]['header']['avg'] = 'avg'
+        sums[queryid]['url1']['total'] = 0
+        sums[queryid]['url1']['avg'] = 0
+
+        printwrite("===============")
+        printwrite(onequery['SPARQL'], verbose)
+
+        tmpSPARQL = onequery['SPARQL']
+        tmpfetchurl = onequery['fetchUrl']
+
+        if (not tmpSPARQL.find('bgeelight') and not tmpfetchurl.find('bgeelight')):
+            printwrite('no bgee involved')
+            continue
+
+        tmpQuestion = onequery['question']
+        for onevariable in onequery['vars']:
+            tmptoken = separator + onevariable['name'] + separator
+            if 'defaultvalue' in onevariable:
+                tmpSPARQL = tmpSPARQL.replace(tmptoken, onevariable['defaultvalue'])
+            else:
+                tmpSPARQL = tmpSPARQL.replace(tmptoken, onevariable['default'])
+            tmpQuestion = tmpQuestion.replace(tmptoken, onevariable['default'])
+        tmpSPARQL = tmpSPARQL.replace(separator + 'innerlimit' + separator, ' ' + addlimits + ' ')
+        tmpSPARQL = tmpSPARQL.replace(separator + 'innerlimit10' + separator, ' ' + addlimits10 + ' ')
+
+        for x in range(loops):
+            # loop over different servers
+            for oneurl in ['url1']:
+
+                # prepare URL
+
+                tmpThisSPARQL = tmpSPARQL.replace(url0, eval(oneurl))
+                tmpFetchURL = onequery['fetchUrlShort'].replace(url0, eval(oneurl))
+                printwrite("===============", verbose)
+                printwrite(tmpThisSPARQL, verbose)
+                tmpThisSPARQL = re.sub('#[^>\n]+\n', '\n', tmpThisSPARQL)
+                printwrite("===============", verbose)
+                printwrite(tmpThisSPARQL, verbose)
+                tmpThisSPARQL = tmpThisSPARQL.replace('\n', ' ')
+                tmpThisSPARQL = tmpThisSPARQL.replace('\t', ' ')
+                tmpThisSPARQL = urllib.parse.quote_plus(tmpThisSPARQL)
+
+                tmpurl = tmpFetchURL + intermed + tmpThisSPARQL + restofurl
+                printwrite("===============", verbose)
+                printwrite(tmpurl, verbose)
+
+                # fetch result
+                tmpres = requests.get(tmpurl, headers={'Cache-Control': 'no-cache'})
+
+                printwrite("===============", verbose)
+                printwrite(tmpres.text, verbose)
+                tmptime = tmpres.elapsed.total_seconds()
+                sums[queryid][oneurl]['total'] = sums[queryid][oneurl]['total'] + tmptime
+                sums[queryid]['header']['p' + str(x)] = x
+                sums[queryid][oneurl]['p' + str(x)] = str(tmptime)
+
+                printwrite("===============", verbose)
+                printwrite("\n", verbose)
+
+                # calculate average
+                sums[queryid][oneurl]['avg'] = sums[queryid][oneurl]['total']/loops
+
+        # write single statistic to console
+        printwrite("===============", 0, "\n", "\n")
+        printwrite("===============", 0, "\n", "\n")
+        printwrite("statistics")
+        printwrite("===============")
+        printwrite(str(loops) + " tries")
+        for qnum, qnumdata in sums[queryid].items():
+            printwrite("", 1)
+            printwrite(eval(qnum + '_title'), 1, ", ")
+            for point, pointdata in qnumdata.items():
+                printwrite(pointdata, 1, ", ")
+        printwrite("", 1)
+        # break # used for debugging
+
+# full statistic at the end
+printwrite("===============")
+printwrite("fullstat")
+printwrite("===============")
+
+for queryid, filedata in sums.items():
+    printwrite(queryid)
+    for qnum, qnumdata in sums[queryid].items():
+        printwrite(eval(qnum + '_title'), 1, ",")
+        for point, pointdata in qnumdata.items():
+            printwrite(pointdata, 1, ",")
+        printwrite("", 1)
+
+printwrite('### this statistics file ist created by bioSODA queryAll.py (https://github.com/biosoda/bioquery/tree/master/statistics) - created at ' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
